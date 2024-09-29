8e2f16ec41b051177f01e5b66113e922babbd2a8
Ferran Llamas <llamas.arroniz@gmail.com>
2024-09-29 17:56:43 +0200

    add numpy

--- similarity.py
+++ similarity.py
@@ -1,12 +1,14 @@
 import math
 import random
 import time
-from nucliadb_node_binding import Similarity
 
+import numpy as np
+
+from nucliadb_node_binding import Similarity
 
 latencies = {}
 
-def measure_latency(func):
+def measure_time(func):
     def wrapper(*args, **kwargs):
         start_time = time.perf_counter()
         result = func(*args, **kwargs)
@@ -16,7 +18,7 @@ def measure_latency(func):
     return wrapper
 
 
-def print_latencies():
+def print_times():
     # Print tabulated table with all latencies
     print("\nLatencies table:")
     print("{:<20} | {:>10} | {:>10} | {:>10}".format("Function", "p50 (ms)", "p90 (ms)", "p99 (ms)"))
@@ -29,13 +31,13 @@ def print_latencies():
         print("{:<20} | {:>10.2f} | {:>10.2f} | {:>10.2f}".format(func_name, p50, p90, p99))
 
 
-@measure_latency
+@measure_time
 def python_dot_product(vec1: list[float], vec2: list[float]) -> float:
     # Calculate the dot product of the two vectors
     return sum(a * b for a, b in zip(vec1, vec2))
 
 
-@measure_latency
+@measure_time
 def python_cosine(vec1: list[float], vec2: list[float]) -> float:
     # Step 1: Calculate the dot product of the two vectors
     dp = python_dot_product(vec1, vec2)
@@ -50,14 +52,24 @@ def python_cosine(vec1: list[float], vec2: list[float]) -> float:
     return dp / (magnitude_vec1 * magnitude_vec2)
 
 
-@measure_latency
+@measure_time
 def rust_dot_product(vec1: list[float], vec2: list[float]) -> float:
     return Similarity().dot_product(vec1, vec2)
 
 
-@measure_latency
+@measure_time
 def rust_cosine(vec1: list[float], vec2: list[float]) -> float:
     return Similarity().cosine(vec1, vec2)
+
+
+@measure_time
+def numpy_dot_product(vec1: list[float], vec2: list[float]) -> float:
+    return np.dot(vec1, vec2)
+
+
+@measure_time
+def numpy_cosine(vec1: list[float], vec2: list[float]) -> float:
+    return np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
 
 
 def main():
@@ -74,8 +86,10 @@ def main():
         python_dot_product(vec1, vec2)
         rust_cosine(vec1, vec2)
         rust_dot_product(vec1, vec2)
+        numpy_cosine(vec1, vec2)
+        numpy_dot_product(vec1, vec2)
 
-    print_latencies()
+    print_times()
 
 
 if __name__ == "__main__":
